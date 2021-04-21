0022d527b62a1a3444975fff67f53144657e5adf
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-04-21 12:12:54 +0300

    the maximum subarray for hackerrank solution

--- graph-algorithms/prims_algorithms_connecting_islands/test.py
+++ graph-algorithms/prims_algorithms_connecting_islands/test.py
@@ -6,7 +6,7 @@ def getMinimumCost(k, c):
     cost = 0
     previous_purchase = 0
     for i in range(len(c)):
-        cost += (previous_purchase +1) * c[i]
+        cost += (previous_purchase + 1) * c[i]
         if (i + 1) % k == 0:
             previous_purchase += 1
     return cost
@@ -43,67 +43,85 @@ def candies(n, arr):
 # print(getMinimumCost(3, [1, 3, 5, 7, 9]))
 
 class Node:
-    def __init__(self, total_sum=0, p_sum=0):
+    def __init__(self, total_sum=0, p_sum=0, is_leaf=False, is_delivery_node=False):
         self.total_sum = total_sum
         self.p_sum = p_sum
+        self.is_leaf = is_leaf
+        self.is_delivery_node = is_delivery_node
 
     def get_p_sum(self):
+        if self.total_sum > self.p_sum:
+            return self.total_sum
+
         return self.p_sum if self.total_sum > 0 else self.total_sum
 
 
 '''Helper Function - Find the max crossing sum w.r.t. middle index'''
+
+
 def maxCrossingSum(arr, start, mid, stop):
     '''LEFT PHASE - Traverse the Left part starting from mid element'''
     leftSum = arr[mid]  # Denotes the sum of left part from mid element to the current element
     leftMaxSum = arr[mid]  # Keep track of maximum sum
-    pLeftSum = arr[mid] if arr[mid] > 0 else 0
 
     # Traverse in reverse direction from (mid-1) to start
     for i in range(mid - 1, start - 1, -1):  # The second argument of range is not inclusive. Third argument is the step size.
         leftSum = leftSum + arr[i]
-        if (arr[i] > 0):
-            pLeftSum += arr[i]
         if (leftSum > leftMaxSum):  # Update leftMaxSum
             leftMaxSum = leftSum
 
     '''RIGHT PHASE - Traverse the Right part, starting from (mid+1)'''
     rightSum = arr[mid + 1]  # Denotes the sum of right part from (mid+1) element to the current element
     rightMaxSum = arr[mid + 1]  # Keep track of maximum sum
-    pRightSum = arr[mid + 1] if arr[mid + 1] > 0 else 0
 
     # Traverse in forward direction from (mid+2) to stop
     for j in range(mid + 2, stop + 1):  # The second argument of range is not inclusive
         rightSum = rightSum + arr[j]
-        if (arr[j] > 0):
-            pRightSum += arr[j]
-
         if (rightSum > rightMaxSum):  # Update rightMaxSum
             rightMaxSum = rightSum
 
     '''Both rightMaxSum and lefttMaxSum each would contain value of atleast one element from the arr'''
 
-    return Node(leftMaxSum + rightMaxSum, pRightSum + pLeftSum)
+    return Node(leftMaxSum + rightMaxSum)
 
 
 '''Recursive function'''
 
+
 def maxSubArrayRecursive(arr, start, stop):  # start and stop are the indices
     # Base case
     if start == stop:
-        return Node(arr[start])
+        return Node(arr[start], 0, True)
 
     if start < stop:
         mid = (start + stop) // 2  # Get the middle index
-        L1 = maxSubArrayRecursive(arr, start, mid)  # Recurse on the Left part
-        R1 = maxSubArrayRecursive(arr, mid + 1, stop)  # Recurse on the Right part
-        C1 = maxCrossingSum(arr, start, mid, stop)  # Find the max crossing sum w.r.t. middle index
+        L = maxSubArrayRecursive(arr, start, mid)  # Recurse on the Left part
+        R = maxSubArrayRecursive(arr, mid + 1, stop)  # Recurse on the Right part
+        C = maxCrossingSum(arr, start, mid, stop)  # Find the max crossing sum w.r.t. middle index
 
-        max_sum = max(C1.total_sum, max(L1.total_sum, R1.total_sum)) # Return the maximum of (L,R,C)
-        if max_sum == L1.total_sum:
-            return L1
-        if max_sum == R1.total_sum:
-            return R1
-        return C1
+        subSeqSum = 0
+        if L.is_leaf and L.total_sum > 0:
+            subSeqSum += L.total_sum
+        if R.is_leaf and R.total_sum > 0:
+            subSeqSum += R.total_sum
+        if C.is_leaf and C.total_sum > 0:
+            subSeqSum += C.total_sum
+
+        delivery_sum = 0
+        if L.is_delivery_node:
+            delivery_sum += L.p_sum
+        if R.is_delivery_node:
+            delivery_sum += R.p_sum
+        if C.is_delivery_node:
+            delivery_sum += C.p_sum
+
+        max_sum = max(C.total_sum, max(L.total_sum, R.total_sum))  # Return the maximum of (L,R,C)
+
+        if max_sum == L.total_sum:
+            return Node(L.total_sum, delivery_sum + subSeqSum, False, True)
+        if max_sum == R.total_sum:
+            return Node(R.total_sum, delivery_sum + subSeqSum, False, True)
+        return Node(C.total_sum, delivery_sum + subSeqSum, False, True)
 
     else:  # If ever start > stop. Not feasible.
         return Node(nums[start])
@@ -119,6 +137,12 @@ def maxSubArray(arr):
 # arr = [-1, -1, -1, -1]
 # arr = [1, 2, 3, 4]
 # arr = [2, -1, 2, 3, 4, -5]
-arr = [-2, -3, -1, -4, -6]
+# arr = [1]
+# arr = [-1, -2, -3, -4, -5, -6]
+# arr = [1, -2]
+# arr = [1, 2, 3]
+# arr = [-10]
+# arr = [1, -1, -1, -1, -1, 5]
+arr = [2, -1, 2, 3, 4, -5]
 node = maxSubArray(arr)
 print("Max1 = ", node.total_sum, " Max2 = ", node.get_p_sum())
